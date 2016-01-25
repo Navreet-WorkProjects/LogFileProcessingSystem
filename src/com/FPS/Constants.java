a0609cf1310794e856224df53836b84c015ef719
navreetKaur <navreet.kaur10@gmail.com>
2016-01-24 20:29:30 -0500

    Create Constants.java

---
+++ src/com/FPS/Constants.java
@@ -0,0 +1,16 @@
+package com.FPS;
+
+public interface Constants {
+	public final String SUCCESS = "success";
+	public final String DIRECTORY = "F:\\InterviewProject";
+	public final int THREADS = 20;
+	public final String FAIL = "fail";
+	public final String DATE_FORMAT = "yyyy-MM-dd";
+	public final String FILE_END = ".log";
+	public final String FILE_START = "logtest.";
+	public final String ENTER_DIR = "Please enter the directory";
+	public final String ENTER_THREAD = "Please enter the number of threads";
+	public final String LINE_NUMBER_FORMAT = "%d.%s%s";
+	
+	
+}
